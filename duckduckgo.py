a2a988b08f8f73ef69b32001f05b3bfefe761c1c
seva1232 <tanatos1232@yandex.ru>
2018-12-08 14:16:38 +0300

    regexp fixes

--- duckduckgo.py
+++ duckduckgo.py
@@ -1,11 +1,11 @@
 import re
-from urllib.parse import quote_plus
+from urllib.parse import quote_plus, unquote_plus
 import aiohttp
 
 
 def ddg_parse(text_site):
     first_link = re.findall(r'(?<=class="result__a" href=").+(?=">)', text_site)[0]
-    first_link = re.findall(r'(?<=http).+', first_link)[0]
+    first_link = re.findall(r'http.+', first_link)[0]
     return first_link
 
 
@@ -17,5 +17,16 @@ async def ddg_search(question):
     async with aiohttp.ClientSession() as session:
         async with session.get(url, headers=headers) as resp:
             site_text = await resp.text()
-            answer = ddg_parse(site_text)
+            answer = ddg_parse(unquote_plus(site_text))
             return answer
+
+
+if __name__ == "__main__":
+    from requests import get
+    question = input(":>")
+    url = 'https://duckduckgo.com/html?q={}'.format(quote_plus(question + " play game "))
+    req = get(url=url)
+    with open("../text.html", "w") as file:
+        file.write(req.text)
+    print(ddg_parse(unquote_plus(req.text)))
+    print(ddg_parse((req.text)))
